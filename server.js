0b240b22b5a82b976c24c1d9d1058ab62a207b9b
Alicia Dale <boricua21dale@gmail.com>
2022-07-25 16:39:04 -0600

    testing host

--- server.js
+++ server.js
@@ -2,7 +2,32 @@
 const express = require('express');
 // Constants
 const PORT = 8080;
-const HOST = 'localhost';
+//const HOST = 'localhost';
+
+// Ecosia Testing
+const HOST = 'http://local.ecosia.org/tree'
+
+//Setting Constant METHOD to get since this what we want as the only allowable method
+const METHOD = 'GET';
+
+// HTTP Requests Allowed
+// Only Accepting GET Requests
+const http = require('http');
+//const request = require('request')
+//constGetReq = {
+//  url: 'http://local.ecosia.org/tree',
+//};
+
+//Return the GET request
+//request.get(GettReq, (err, res, body) => {
+//    if (err) {
+//        return console.log(err);
+//    }
+
+//    console.log(body);
+//    console.log(`You have successfully made the post request`);
+//});
+
 // App
 const app = express();
 app.get('/', (req, res) => {
